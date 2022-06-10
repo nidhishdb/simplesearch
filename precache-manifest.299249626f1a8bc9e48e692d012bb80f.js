5c25068ab8c9ea7a40971b900df3b7a5a3552ebb
nidhishdb <nidhishdb@gmail.com>
2022-06-10 19:30:20 +0530

    Updates

---
+++ precache-manifest.299249626f1a8bc9e48e692d012bb80f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966e8e241be3e06074ee72d0dd25296b",
+    "url": "/simplesearch/index.html"
+  },
+  {
+    "revision": "6781d13d089915b4b957",
+    "url": "/simplesearch/static/css/main.5bf4b6f4.chunk.css"
+  },
+  {
+    "revision": "f4af145a140da55b4fe5",
+    "url": "/simplesearch/static/js/2.50a855a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simplesearch/static/js/2.50a855a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6781d13d089915b4b957",
+    "url": "/simplesearch/static/js/main.6560af91.chunk.js"
+  },
+  {
+    "revision": "961380e46907416842c7",
+    "url": "/simplesearch/static/js/runtime-main.2b19ccff.js"
+  }
+]);
